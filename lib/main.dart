cd4558fea9d1fefaa936e445e99249007dfab630
hassan-SS-cs <has597962@gmail.com>
2026-06-14 22:17:29 +0300

    Add audio recording and playback features;

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:x_events/widgets/app_navigation.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
   runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: const AppNavigation(),
-    ),
+    const MaterialApp(debugShowCheckedModeBanner: false, home: AppNavigation()),
   );
+
+  FlutterNativeSplash.remove();
 }
